37e4b6a0d4d2cf6648b59340c9dee7e278fad8d4
Chris Angelico <rosuav@gmail.com>
2015-02-21 21:49:03 +1100

    Initial commit

---
+++ keys_sample.py
@@ -0,0 +1,3 @@
+# Copy this file to keys.py and enter the appropriate keys as assigned by Google.
+CLIENT_ID = ""
+CLIENT_SECRET = ""
---
+++ letmeknow.py
@@ -0,0 +1,52 @@
+from __future__ import print_function
+import sys
+import argparse
+import httplib2
+import oauth2client.file
+import oauth2client.client
+import googleapiclient.discovery
+
+from keys import * # ImportError? Check out keys_sample.py for details.
+
+storage = oauth2client.file.Storage("credentials.dat")
+credentials = storage.get()
+if not credentials or credentials.invalid:
+	# HACK: Use the run_flow function to save some trouble, but don't
+	# actually pass it any of the args from the command line. TODO: Use
+	# our own code here instead.
+	flow = oauth2client.client.OAuth2WebServerFlow(client_id=CLIENT_ID,client_secret=CLIENT_SECRET,
+		scope='https://www.googleapis.com/auth/calendar.readonly', # Don't need any read/write access
+		user_agent='Let Me Know')
+	flags=argparse.Namespace(auth_host_name='localhost', auth_host_port=[8080, 8090], logging_level='ERROR', noauth_local_webserver=False)
+	credentials = oauth2client.tools.run_flow(flow, storage, flags)
+# At this point, we should have viable credentials.
+service = googleapiclient.discovery.build("calendar", "v3", http=credentials.authorize(http=httplib2.Http()))
+
+parser = argparse.ArgumentParser(description="Let Me Know - Google Calendar notifications using Frozen")
+subparsers = parser.add_subparsers(dest="command", help="Available commands")
+
+def command(f):
+	doc = f.__doc__
+	if not doc: doc=""
+	doc = doc.split("\n")
+	p = subparsers.add_parser(f.__name__, help=doc[0])
+	for arg in doc[1:]:
+		arg = arg.strip().split(":", 1)
+		if len(arg) < 2: continue # Blank lines etc
+		p.add_argument(arg[0].strip(), help=arg[1].strip())
+	return f
+
+@command
+def list():
+	"""List calendars available to your account"""
+	page_token = None
+	while True:
+		calendar_list = service.calendarList().list(pageToken=page_token).execute()
+		for calendar_list_entry in calendar_list['items']:
+			print(calendar_list_entry['summary'])
+		page_token = calendar_list.get('nextPageToken')
+		if not page_token: break
+
+if __name__ == "__main__":
+	arguments = parser.parse_args(sys.argv[1:]).__dict__
+	globals()[arguments.pop("command")](**arguments)
