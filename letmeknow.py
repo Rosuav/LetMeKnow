06bcc29627a31a008af311ea38c37ee0ac978eac
Chris Angelico <rosuav@gmail.com>
2015-02-21 22:12:52 +1100

    Move authentication into a function

--- letmeknow.py
+++ letmeknow.py
@@ -1,29 +1,34 @@
 from __future__ import print_function
 import sys
 import argparse
-import httplib2
-import oauth2client.file
-import oauth2client.client
-import googleapiclient.discovery
 
 from keys import * # ImportError? Check out keys_sample.py for details.
 
-storage = oauth2client.file.Storage("credentials.dat")
-credentials = storage.get()
-if not credentials or credentials.invalid:
-	# HACK: Use the run_flow function to save some trouble, but don't
-	# actually pass it any of the args from the command line. TODO: Use
-	# our own code here instead.
-	flow = oauth2client.client.OAuth2WebServerFlow(client_id=CLIENT_ID,client_secret=CLIENT_SECRET,
-		scope='https://www.googleapis.com/auth/calendar.readonly', # Don't need any read/write access
-		user_agent='Let Me Know')
-	flags=argparse.Namespace(auth_host_name='localhost', auth_host_port=[8080, 8090], logging_level='ERROR', noauth_local_webserver=False)
-	credentials = oauth2client.tools.run_flow(flow, storage, flags)
-# At this point, we should have viable credentials.
-service = googleapiclient.discovery.build("calendar", "v3", http=credentials.authorize(http=httplib2.Http()))
-
 parser = argparse.ArgumentParser(description="Let Me Know - Google Calendar notifications using Frozen")
 subparsers = parser.add_subparsers(dest="command", help="Available commands")
+
+def auth():
+	"""Authenticate with Google. Must be done prior to any other commands."""
+	# Some of these imports take quite a while, so don't do them if the user
+	# asks for --help or somesuch.
+	import httplib2
+	import oauth2client.file
+	import oauth2client.client
+	import googleapiclient.discovery
+	storage = oauth2client.file.Storage("credentials.dat")
+	credentials = storage.get()
+	if not credentials or credentials.invalid:
+		# HACK: Use the run_flow function to save some trouble, but don't
+		# actually pass it any of the args from the command line. TODO: Use
+		# our own code here instead.
+		flow = oauth2client.client.OAuth2WebServerFlow(client_id=CLIENT_ID,client_secret=CLIENT_SECRET,
+			scope='https://www.googleapis.com/auth/calendar.readonly', # Don't need any read/write access
+			user_agent='Let Me Know')
+		flags=argparse.Namespace(auth_host_name='localhost', auth_host_port=[8080, 8090], logging_level='ERROR', noauth_local_webserver=False)
+		credentials = oauth2client.tools.run_flow(flow, storage, flags)
+	# At this point, we should have viable credentials.
+	global service
+	service = googleapiclient.discovery.build("calendar", "v3", http=credentials.authorize(http=httplib2.Http()))
 
 def command(f):
 	doc = f.__doc__
@@ -49,4 +54,5 @@ def list():
 
 if __name__ == "__main__":
 	arguments = parser.parse_args(sys.argv[1:]).__dict__
+	auth()
 	globals()[arguments.pop("command")](**arguments)
