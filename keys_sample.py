ff223f828bd06b6ff85236c49d74ebb05e49022c
Chris Angelico <rosuav@gmail.com>
2017-04-28 05:15:23 +1000

    Add some docos to the sample file

--- keys_sample.py
+++ keys_sample.py
@@ -1,4 +1,6 @@
 # Copy this file to keys.py and enter the appropriate keys as assigned by Google.
+# Go to https://console.developers.google.com/ and create an app, then enable the
+# Google Calendar API for it. Obtain OAuth2 credentials and record them here.
 CLIENT_ID = ""
 CLIENT_SECRET = ""
 DEFAULT_CALENDAR = "" # If nonblank, will be the default for all 'calendar' args
